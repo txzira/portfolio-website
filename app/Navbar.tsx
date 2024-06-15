fd8bcba6c524ba99191e330b125962e57f224309
ronnie <ronnsr15@gmail.com>
2024-06-15 00:01:34 -0700

    Implement Navbar, about page, contact page, and projects page

---
+++ app/Navbar.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul className="flex flex-row">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
