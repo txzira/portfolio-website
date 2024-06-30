99230806f54d80689fc06504d595ef31c3a1fc0d
txzira <txziraio@gmail.com>
2024-06-30 04:09:50 -0700

    center navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,23 +5,25 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg sticky-top bg-primary-subtle">
-      <ul className="navbar-nav fw-semibold">
-        <li className="nav-item nav-underline">
-          <Link href="#about" className="nav-link ">
-            About
-          </Link>
-        </li>
-        <li className="nav-item nav-underline">
-          <Link href="#projects" className="nav-link">
-            Projects
-          </Link>
-        </li>
-        <li className="nav-item nav-underline">
-          <Link href="#socials" className="nav-link">
-            Socials
-          </Link>
-        </li>
-      </ul>
+      <div className="container">
+        <ul className="navbar-nav fw-semibold">
+          <li className="nav-item nav-underline">
+            <Link href="#about" className="nav-link ">
+              About
+            </Link>
+          </li>
+          <li className="nav-item nav-underline">
+            <Link href="#projects" className="nav-link">
+              Projects
+            </Link>
+          </li>
+          <li className="nav-item nav-underline">
+            <Link href="#socials" className="nav-link">
+              Socials
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
